192891809841f975a944bc9888bb0a36474bb2c2
AkinniyiS <samuel30274@gmail.com>
2025-03-22 16:28:48 -0400

    added color

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+    primaryColor: const Color.fromARGB(255, 2, 75, 20), // Color for AppBar and other elements
+    scaffoldBackgroundColor: const Color.fromARGB(255, 84, 226, 18), // Background color for screens
+    colorScheme: ColorScheme.fromSwatch().copyWith(secondary: const Color.fromARGB(255, 63, 23, 205)), // Accent color
+  ),
       home: LoginScreen(),
     );
   }
